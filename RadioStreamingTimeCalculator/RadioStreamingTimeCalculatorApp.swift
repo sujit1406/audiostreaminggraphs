63d25e077d0d6e45baf469d41512de63ff6e41b0
Sujith Antony <mail2say@gmail.com>
2024-09-04 10:55:31 +0530

    Initial Commit

---
+++ RadioStreamingTimeCalculator/RadioStreamingTimeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioStreamingTimeCalculatorApp.swift
+//  RadioStreamingTimeCalculator
+//
+//  Created by Sujith Antony on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RadioStreamingTimeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
